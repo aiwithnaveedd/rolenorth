0221cb0de9b460ed18f9f7e39147c73da616e23f
Naveedahmedgopang76 <naveedahmedgopang76@gmail.com>
2026-05-16 15:01:38 +0500

    add redirect url

---
+++ app/(auth)/callback/route.ts
@@ -0,0 +1,16 @@
+import { createClientServer } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+
+  if (code) {
+    const supabase = await createClientServer();
+
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // Redirect to dashboard after login
+  return NextResponse.redirect(new URL("/dashboard", requestUrl.origin));
+}
